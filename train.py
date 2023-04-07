971e595d85e07d1ac2ecd4f248774e8ed3a8efa2
sushant097 <sushant1234gautam@gmail.com>
2023-04-07 22:06:43 +0545

    added hyperparameters

--- train.py
+++ train.py
@@ -13,3 +13,8 @@ from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
 
+model_name = "regnetz_c16"
+optimizer_name = "ADAM"
+learning_rate = 0.000012
+batch_size = 64
+
