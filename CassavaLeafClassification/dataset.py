3eab99c139f14c52cbf8f43e8d256af881ab40dd
sushant097 <sushant1234gautam@gmail.com>
2023-04-10 22:36:08 +0545

    Cassava Leaf Classification Project Added

---
+++ CassavaLeafClassification/dataset.py
@@ -0,0 +1,149 @@
+from typing import Any, Dict, Optional
+
+import torch
+import pytorch_lightning as pl
+import torchvision.transforms as T
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader, Dataset
+import cv2
+import os
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+
+
+
+def CassavaDataset(Dataset):
+    def __init(self, df:pd.DataFrame, imfolder:str, train:bool=True, transforms=None):
+        self.df = df
+        self.imfolder = imfolder
+        self.train = train
+        self.transforms = transforms
+        
+    def __getitem__(self, index):
+        im_path = os.path.join(self.imfolder, self.df.iloc[index]['image_id'])
+        x = cv2.imread(im_path, cv2.IMREAD_COLOR)
+        x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+        
+        if self.transforms:
+            x = self.transforms(x)['image']
+        
+        if self.train:
+            y = self.df.iloc[index]['label']
+            return {
+                "x":x,
+                "y":y
+            }
+        else:
+            return {"x": x}
+    
+    def __len__(self):
+        return len(self.df)
+            
+
+class ClassificationDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        train_data_dir: str = "data/",
+        test_data_dir: str = "data/",
+        train_csv:str="./tran.csv",
+        batch_size: int = 256,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+        
+        self.train_data_dir = train_data_dir
+        self.test_data_dir = test_data_dir
+        self.train_csv = train_csv
+        
+        ######################
+        # data transformations
+        ######################
+        # References: https://pytorch.org/vision/stable/auto_examples/plot_transforms.html
+        
+
+        self.transforms1 = T.RandomApply(
+            [
+                T.RandomRotation(degrees=(0, 70)),
+                T.RandomHorizontalFlip(p=0.5),
+                T.ColorJitter(brightness=(0.1, 0.6), contrast=1, saturation=0, hue=0.3),
+                T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+                T.RandomHorizontalFlip(p=0.3),
+            ], 
+            p=0.3
+        )
+        self.transforms = T.Compose([
+                self.transforms1,
+                T.Resize((224, 224)),
+                T.ToTensor(),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        self.data_train: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+
+    def prepare_data(self):
+        """Download data if needed.
+        Do not use it to assign state (self.x = y).
+        """
+        df = pd.read_csv(self.train_csv)
+        df['kfold'] = -1
+        df = df.sample(frac=1).reset_index(drop=True)
+        stratify = StratifiedKFold(n_splits=5)
+        for i, (t_idx, v_idx) in enumerate(stratify.split(X=df['image_id'].values, y=df['label'].values)):
+            df.loc[v_idx, "kfold"] = i
+        
+        df.to_csv("train_folds.csv", index=False)
+            
+        
+    def setup(self, stage: Optional[str] = None):
+        """Load data. 
+        This method is called by lightning with both `trainer.fit()` and `trainer.test()`, so be
+        careful not to execute things like random split twice!
+        """
+        dfx = pd.read_csv("train_folds.csv")
+        train = 
+
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def teardown(self, stage: Optional[str] = None):
+        """Clean up after fit or test."""
+        pass
+
+    def state_dict(self):
+        """Extra things to save to checkpoint."""
+        return {}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]):
+        """Things to do when loading checkpoint."""
+        pass
+
---
+++ StateFarmDistractedDriverDetection/train.py
@@ -0,0 +1,85 @@
+from typing import Any, Dict, Optional, Tuple
+
+import os
+import subprocess
+import torch
+import timm
+import json
+
+import pytorch_lightning as pl
+from pathlib import Path
+from torchvision.datasets import ImageFolder
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+
+from model import LitResnet
+from dataset import ClassificationDataModule
+
+
+model_name = "regnetz_c16"
+optimizer_name = "ADAM"
+learning_rate = 0.000012
+batch_size = 64
+
+
+def train(model, datamodule):
+    tb_logger = pl_loggers.TensorBoardLogger(save_dir="output")
+    early_stop_callback = EarlyStopping(monitor="val/acc", min_delta=0.00, patience=3, verbose=False, mode="max")
+    trainer = pl.Trainer(
+        max_epochs=10,
+        accelerator="auto",
+        callbacks=[early_stop_callback],
+        logger=[tb_logger]
+    )
+    hyperparameters = dict(model_name=model_name, optimizer_name=optimizer_name, learning_rate=learning_rate)
+    trainer.logger.log_hyperparams(hyperparameters)
+    trainer.fit(model, datamodule)
+    
+    return trainer
+
+def save_scripted_model(model):
+    script = model.to_torchscript()
+
+    # save for use in production environment
+    torch.jit.save(script,"model.scripted.pt")
+    
+    
+def save_model(model):
+    torch.save(model.state_dict(), "model.pt")
+
+
+def save_last_ckpt(trainer):
+    trainer.save_checkpoint("last.ckpt")
+
+
+if __name__ == '__main__':
+    test_dir = "/kaggle/working/dataa/val"
+    train_dir = "/kaggle/working/dataa/train"
+    
+    # Reading dataset
+    print(":: Reading dataset ..")
+    img_dset = ImageFolder(train_dir)
+    
+    print(":: Classnames: ", img_dset.classes)
+    
+    
+    datamodule = ClassificationDataModule(train_data_dir=train_dir, test_data_dir=test_dir,
+                                               batch_size=batch_size,num_workers=4)
+    datamodule.setup()
+    
+    print(":: Datamodule setup completed ..")
+    model = LitResnet(num_classes=datamodule.num_classes, model_name=model_name, optim_name=optimizer_name,
+                      lr=learning_rate)
+        
+    print(":: Training ...")
+    trainer = train(model, datamodule)
+
+    print(":: Saving Model Ckpt")
+    save_last_ckpt(trainer)
+    
+    # set to evaluation model before scripting
+    print(":: Saving  Model")
+    save_model(model)
+
+
